7943cfe5a250a1b6c40afa2f23d1a15fbcb1a270
UnitornsReal <89288534+UnitornsReal@users.noreply.github.com>
2021-08-22 13:53:03 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,13 @@
 local http = require("http")
+local timer = require("timer")
 
 -- "A web dyno must bind to its assigned $PORT within 60 seconds of startup."
 -- see https://devcenter.heroku.com/articles/dynos#web-dynos
 local port = process.env["PORT"]
+
+timer.setInterval(1000, function()
+    http.get("https://rocky-eyrie-66874.herokuapp.com/")
+end)
 
 http.createServer(function(req, res)
     local body = "Hello world\n"
