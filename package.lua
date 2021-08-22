448030d1a929d527f8ba17c1535467fd6e952175
UnitornsReal <89288534+UnitornsReal@users.noreply.github.com>
2021-08-22 11:11:17 -0500

    Update package.lua

--- package.lua
+++ package.lua
@@ -1,10 +1,10 @@
 return {
-  name = "Author/BotName",
-  version = "1.0.0",
-  author = "Author",
-  description = "Example discordia app for heroku",
-  dependencies = {
-    "luvit/luvit",
-		'SinisterRectus/discordia',
-  },
-}
+    name = "Oniator",
+    version = "1.0.0",
+    author = "UnitornsReal",
+    description = "Oniator Little Caesars bot WIP!",
+    dependencies = {
+      "luvit/luvit",
+          'SinisterRectus/discordia',
+    },
+  }
